c6ae33f2a32030227d5a883c24e14f5d6152eb6c
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-12-14 11:35:00 +0000

    tests added

--- postcodesio/postcodesio_httparty.rb
+++ postcodesio/postcodesio_httparty.rb
@@ -5,10 +5,58 @@ class Postcodesio
 
   include HTTParty
 
+  @postcode_array = []
+
   base_uri "https://api.postcodes.io"
 
   def get_single_postcode(postcode)
+    JSON.parse(self.class.get("/postcodes/#{postcode}").body)
+  end
 
+  def get_multiple_postcodes(postcodes)
+    JSON.parse(self.class.post("/postcodes", body:{"postcodes" => postcodes}).body)
+  end
+
+  def get_single_status(postcode)
+    get_single_postcode(postcode)["status"]
+  end
+
+  def get_multiple_status(postcodes)
+    get_multiple_postcodes(postcodes)["status"]
+  end
+
+  def get_postcode_result(postcode)
+    get_single_postcode(postcode)["result"]
+  end
+
+  def get_postcodes_results(postcodes)
+    get_multiple_postcodes(postcodes)["result"]
+  end
+
+  def get_postcode(postcode)
+    get_postcode_result(postcode)["postcode"]
+  end
+
+  def get_single_quality(postcode)
+    get_postcode_result(postcode)["quality"]
+  end
+
+  def get_single_eastings(postcode)
+    get_postcode_result(postcode)["eastings"]
+  end
+
+  def get_single_northings(postcode)
+    get_postcode_result(postcode)["northings"]
+  end
+
+  def get_single_country(postcode)
+    get_postcode_result(postcode)["country"]
   end
 
 end
+
+x=Postcodesio.new
+
+# puts x.get_postcodes(["b601ja", "tq60nr", "ox27dr"])
+# puts x.get_single_postcode("b601ja")
+# puts x.get_multiple_postcodes(["b601ja", "tq60nr", "ox27dr"])["result"]
---
+++ postcodesio/spec/postcodesio_httparty_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe "Testing postcodesio" do
+
+  before(:each) do
+    @file = Postcodesio.new
+  end
+
+  a = "b601ja"
+  b = ["b601ja", "tq60nr", "ox27dr"]
+
+  it 'Should be a Hash' do
+    expect(@file.get_single_postcode(a)).to be_kind_of(Hash)
+    expect(@file.get_multiple_postcodes(b)).to be_kind_of(Hash)
+
+  end
+
+  it "should receive a status code of 200" do
+    expect(@file.get_single_status(a)).to eq 200
+    expect(@file.get_multiple_status(b)).to eq 200
+  end
+
+  it "should have a postcode that is a String and that it is 7-8 characters long" do
+    expect(@file.get_postcode(a).length).to be_between(6, 8).inclusive
+    expect(@file.get_postcode(a)).to be_kind_of(String)
+
+    @file.get_postcodes_results(b).each do |postcode|
+      expect(postcode["result"]["postcode"].length).to be_between(6, 8).inclusive
+      expect(postcode["result"]).to be_kind_of(Hash)
+    end
+  end
+
+  it "should have a position quality that is an integer from 1 to 9" do
+    expect(@file.get_single_quality(a)).to be_between(1, 9).inclusive
+
+    @file.get_postcodes_results(b).each do |postcode|
+      expect(postcode["result"]["quality"]).to be_between(1, 9).inclusive
+    end
+  end
+
+  it "should have eastings that is an integer" do
+    if @file.get_postcode(a).include?("GY") || @file.get_postcode(a).include?("IM")
+      expect(@file.get_single_eastings(a)).to be_nil
+    else
+      expect(@file.get_single_eastings(a)).to be_kind_of(Integer)
+      @file.get_postcodes_results(b).each do |postcode|
+        expect(postcode["result"]["eastings"]).to be_kind_of(Integer)
+      end
+    end
+  end
+
+  it "should have northings that is an integer" do
+    if @file.get_postcode(a).include?("GY") || @file.get_postcode(a).include?("IM")
+      expect(@file.get_single_northings(a)).to be_nil
+    else
+      expect(@file.get_single_northings(a)).to be_kind_of(Integer)
+      @file.get_postcodes_results(b).each do |postcode|
+        expect(postcode["result"]["northings"]).to be_kind_of(Integer)
+      end
+    end
+  end
+
+  it "should have a country that is a string" do
+    expect(@file.get_single_country(a)).to be_kind_of(String)
+    @file.get_postcodes_results(b).each do |postcode|
+      expect(postcode["result"]["country"]).to be_kind_of(String)
+    end
+  end
+
+end
